5e3a91fcabc4abf783eab571ca65de4581d58ace
bekoinettien <bekoinettien@gmail.com>
2025-08-23 15:33:30 +0000

    gestion du dashboard

--- resources/views/pages/actualite.blade.php
+++ resources/views/pages/actualite.blade.php
@@ -75,45 +75,5 @@
 </section>
 
 <br>
-<section class="map-section container-fluid">
-  <div class="text-center">
-    <h2>Carte des communes d’Abidjan</h2>
-    <div id="map" style="height: 500px; width: 100%;"></div>
-  </div>
 
-<script>
-    // Initialiser la carte centrée sur Abidjan
-    var map = L.map('map').setView([5.348, -4.027], 11);
-
-    // Ajouter fond OpenStreetMap
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '© OpenStreetMap contributors'
-    }).addTo(map);
-
-    // Communes d'Abidjan avec coordonnées approximatives
-    var communes = [
-        { name: "Yopougon", lat: 5.339, lng: -4.082 },
-        { name: "Cocody", lat: 5.354, lng: -3.981 },
-        { name: "Treichville", lat: 5.302, lng: -4.012 },
-        { name: "Marcory", lat: 5.311, lng: -3.979 },
-        { name: "Adjame", lat: 5.359, lng: -4.031 },
-        { name: "Plateau", lat: 5.322, lng: -4.020 },
-        { name: "Koumassi", lat: 5.289, lng: -3.971 },
-        { name: "Port-Bouët", lat: 5.278, lng: -3.944 },
-        { name: "Abobo", lat: 5.418, lng: -4.033 }
-    ];
-
-    // Ajouter les marqueurs pour chaque commune
-    communes.forEach(function(commune) {
-        L.marker([commune.lat, commune.lng])
-            .addTo(map)
-            .bindPopup("<b>" + commune.name + "</b>");
-    });
-
-    // Ajuster la carte pour voir toutes les communes
-    var group = L.featureGroup(communes.map(c => L.marker([c.lat, c.lng])));
-    map.fitBounds(group.getBounds());
-</script>
-
-</section>
 @endsection
